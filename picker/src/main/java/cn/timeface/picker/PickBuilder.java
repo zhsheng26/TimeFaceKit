c9500d80ec0dc2c6c55a331376ec067890aa833e
zhangsheng <zhsheng26@gmail.com>
2017-05-16 17:03:08 +0800

    init

---
+++ picker/src/main/java/cn/timeface/picker/PickBuilder.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2017 Zhihu Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.timeface.picker;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+
+import cn.timeface.picker.ui.PickActivity;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Entry for PickBuilder's media selection.
+ */
+public final class PickBuilder {
+
+    private final WeakReference<Activity> mContext;
+    private final WeakReference<Fragment> mFragment;
+
+    private PickBuilder(Activity activity) {
+        this(activity, null);
+    }
+
+    private PickBuilder(Fragment fragment) {
+        this(fragment.getActivity(), fragment);
+    }
+
+    private PickBuilder(Activity activity, Fragment fragment) {
+        mContext = new WeakReference<>(activity);
+        mFragment = new WeakReference<>(fragment);
+    }
+
+    /**
+     * Start PickBuilder from an Activity.
+     * <p>
+     * This Activity's {@link Activity#onActivityResult(int, int, Intent)} will be called when user
+     * finishes selecting.
+     *
+     * @param activity Activity instance.
+     * @return PickBuilder instance.
+     */
+    public static PickBuilder from(Activity activity) {
+        return new PickBuilder(activity);
+    }
+
+    /**
+     * Start PickBuilder from a Fragment.
+     * <p>
+     * This Fragment's {@link Fragment#onActivityResult(int, int, Intent)} will be called when user
+     * finishes selecting.
+     *
+     * @param fragment Fragment instance.
+     * @return PickBuilder instance.
+     */
+    public static PickBuilder from(Fragment fragment) {
+        return new PickBuilder(fragment);
+    }
+
+    /**
+     * Obtain user selected media' {@link Uri} list in the starting Activity or Fragment.
+     *
+     * @param data Intent passed by {@link Activity#onActivityResult(int, int, Intent)} or
+     *             {@link Fragment#onActivityResult(int, int, Intent)}.
+     * @return User selected media' {@link Uri} list.
+     */
+    public static List<Uri> obtainResult(Intent data) {
+        return data.getParcelableArrayListExtra(PickActivity.EXTRA_RESULT_SELECTION);
+    }
+
+    /**
+     * MIME types the selection constrains on.
+     * <p>
+     * Types not included in the set will still be shown in the grid but can't be chosen.
+     *
+     * @param mimeTypes MIME types set user can choose from.
+     * @return {@link SelectionCreator} to build select specifications.
+     * @see MimeType
+     * @see SelectionCreator
+     */
+    public SelectionCreator choose(Set<MimeType> mimeTypes) {
+        return this.choose(mimeTypes, true);
+    }
+
+    /**
+     * MIME types the selection constrains on.
+     * <p>
+     * Types not included in the set will still be shown in the grid but can't be chosen.
+     *
+     * @param mimeTypes          MIME types set user can choose from.
+     * @param mediaTypeExclusive Whether can choose images and videos at the same time during one single choosing
+     *                           process. true corresponds to not being able to choose images and videos at the same
+     *                           time, and false corresponds to being able to do this.
+     * @return {@link SelectionCreator} to build select specifications.
+     * @see MimeType
+     * @see SelectionCreator
+     */
+    public SelectionCreator choose(Set<MimeType> mimeTypes, boolean mediaTypeExclusive) {
+        return new SelectionCreator(this, mimeTypes, mediaTypeExclusive);
+    }
+
+    @Nullable
+    Activity getActivity() {
+        return mContext.get();
+    }
+
+    @Nullable
+    Fragment getFragment() {
+        return mFragment != null ? mFragment.get() : null;
+    }
+
+}
